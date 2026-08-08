49a34dad3e8835076050c864e3d71948b5b03fb9
duscymod <cymod@foxmail.com>
2026-08-08 11:36:32 +0800

    Update douban_splash.js

--- douban_splash.js
+++ douban_splash.js
@@ -1,18 +1,19 @@
 let body = $response.body;
 
 try {
-
     let obj = JSON.parse(body);
 
+    // 清空豆瓣开屏广告列表
     obj.ads = [];
 
+    // 清空预加载广告 ID
     obj.preload_ids = null;
 
     $done({
         body: JSON.stringify(obj)
     });
 
-} catch(e) {
-
+} catch (e) {
+    console.log("Douban splash filter error: " + e);
     $done({});
 }
